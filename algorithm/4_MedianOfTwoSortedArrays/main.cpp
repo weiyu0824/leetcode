f5e72e165d4367259d0dcd03caff2d86affff95a
weiyu0824 <jcab1688@gmail.com>
2021-12-29 01:58:36 +0800

    Add new question

---
+++ algorithm/4_MedianOfTwoSortedArrays/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+/*
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).
+*/
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
+
+int main (){
+    Solution solution;
+    vector<int> nums1 {1,2};
+    vector<int> nums2 {3,4};
+    cout << solution.findMedianSortedArrays(nums1, nums2);
+
+}
