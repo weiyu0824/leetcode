e6d746396cb06938ae342fb280f92a1c7b37f9e3
weiyu <jcab1688@gmail.com>
2022-05-14 15:56:30 +0800

    finish 70

---
+++ algorithm/DynamicProgramming/70.climbing-stairs/main.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+        int step[n];
+        step[0] = 1; 
+        step[1] = 2;
+        for (int i = 2; i < n; i++){
+            step[i] = step[i - 1] + step[i - 2];
+        }
+        return step[n - 1];
+    }
+};
